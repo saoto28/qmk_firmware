63804f2ef07f8753cb5e07fa78c2ea293bb7020e
saoto tsuchiya <saoto.tsuchiya@gmail.com>
2025-06-01 14:25:17 +0900

    mod keymap of pineapple/p60s

--- keyboards/pineapple/p60s/keymaps/default/keymap.c
+++ keyboards/pineapple/p60s/keymaps/default/keymap.c
@@ -16,21 +16,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,-----------------------------------------.   ,-------------------------------------------------------.
    * | ESC  |  1/! |  2/@ |  3/# |  4/$ |  5/% |   |  6/^ |  7/& | 8/ * |  9/( |  O/) |  -/_ | =/+  | Bksp |
    * |------+------+------+------+------+------|   |------|------+------+------+------+------+------+------.
-   * | Tab  |  q/Q |  w/W |  e/E |  r/R |  t/T |   |  y/Y |  u/U |  i/I |  o/O |  p/P |  [/{ | ]/}  | \/|  |
+   * |  `   |  q/Q |  w/W |  e/E |  r/R |  t/T |   |  y/Y |  u/U |  i/I |  o/O |  p/P |  [/{ | ]/}  | \/|  |
    * |------+------+------+------+------+------|   |------|------+------+------+------+------+-------------.       ------.
-   * | Caps |  a/A |  s/S |  d/D |  f/F |  g/G |   |  h/H |  j/J |  k/K |  l/L |  ;/: |  '/" |Enter |               PgDn |
+   * | Tab  |  a/A |  s/S |  d/D |  f/F |  g/G |   |  h/H |  j/J |  k/K |  l/L |  ;/: |  '/" |Enter |               PgDn |
    * |------+------+------+------+------+------|   |------+------+------+------+------+------+------+-------------
    * |LShift|  z/Z |  x/X |  c/C |  v/V |  b/B |   |  n/N |  m/M |  ,/< |  ./> |  //? |RShift|      | PgUp |  Up  |
    * |------+------+------+------+------+------|   |------+------+------+------+------+------+      +------+------+------.
-   * | LCTRL|FNC/ｱ | LGUI | Alt  | Space|    |Space| NUM  | Alt  | RCTRL|      |   `  |  Del |      | Left | Down | Right|
+   * | LCTRL|FNC/ｱ | LGUI | Alt  | Space|    |Space| NUM  | Alt  | RCTRL|      | Caps |  Del |      | Left | Down | Right|
    * `----------------------------------'    `---------------------------      ---------------      ---------------------.
    */
     [_BASE] = LAYOUT(
       KC_ESC,   KC_1,    KC_2,      KC_3,     KC_4,            KC_5,     KC_6,   KC_7,           KC_8,    KC_9,    KC_0,     KC_MINS, KC_EQL,  KC_BSPC,
-      KC_TAB,   KC_Q,    KC_W,      KC_E,     KC_R,            KC_T,     KC_Y,   KC_U,           KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
-      KC_CAPS,  KC_A,    KC_S,      KC_D,     KC_F,            KC_G,     KC_H,   KC_J,           KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,  KC_PGDN,
+      KC_GRV,   KC_Q,    KC_W,      KC_E,     KC_R,            KC_T,     KC_Y,   KC_U,           KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
+      KC_TAB,  KC_A,    KC_S,      KC_D,     KC_F,            KC_G,     KC_H,   KC_J,           KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,  KC_PGDN,
       KC_LSFT,  KC_Z,    KC_X,      KC_C,     KC_V,            KC_B,     KC_N,   KC_M,           KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_PGUP, KC_UP,
-      KC_LCTL,  LT(_FNC,A(KC_GRV)), KC_LGUI, KC_LALT, KC_SPC, KC_SPC, LT(_NUM,JP_MHEN), KC_RALT, KC_RCTL, KC_GRV,  KC_DEL,   KC_LEFT, KC_DOWN, KC_RGHT
+      KC_LCTL,  LT(_FNC,A(KC_GRV)), KC_LGUI, KC_LALT, KC_SPC, KC_SPC, LT(_NUM,JP_MHEN), KC_RALT, KC_RCTL, KC_CAPS,  KC_DEL,   KC_LEFT, KC_DOWN, KC_RGHT
     ),
  /* Fnc
    * ,-----------------------------------------.   ,------------------------------------------------.
