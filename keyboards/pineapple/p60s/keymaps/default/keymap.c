3d9f8b043a33c00148165121375247f1c6698edc
saoto tsuchiya <saoto.tsuchiya@gmail.com>
2025-06-03 21:34:54 +0900

    mod keymap of p60s

--- keyboards/pineapple/p60s/keymaps/default/keymap.c
+++ keyboards/pineapple/p60s/keymaps/default/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|   |------+------+------+------+------+------+------+-------------
    * |LShift|  z/Z |  x/X |  c/C |  v/V |  b/B |   |  n/N |  m/M |  ,/< |  ./> |  //? |RShift|      | PgUp |  Up  |
    * |------+------+------+------+------+------|   |------+------+------+------+------+------+      +------+------+------.
-   * | LCTRL|FNC/ｱ | LGUI | Alt  | Space|    |Space| NUM  | Alt  | RCTRL|      | Caps |  Del |      | Left | Down | Right|
+   * | LCTRL|NUM/ｱ | LGUI | Alt  | Space|    |Space| FNC  | Alt  | RCTRL|      | Caps |  Del |      | Left | Down | Right|
    * `----------------------------------'    `---------------------------      ---------------      ---------------------.
    */
     [_BASE] = LAYOUT(
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,   KC_Q,    KC_W,      KC_E,     KC_R,            KC_T,     KC_Y,   KC_U,           KC_I,    KC_O,    KC_P,     KC_LBRC, KC_RBRC, KC_BSLS,
       KC_TAB,  KC_A,    KC_S,      KC_D,     KC_F,            KC_G,     KC_H,   KC_J,           KC_K,    KC_L,    KC_SCLN,  KC_QUOT, KC_ENT,  KC_PGDN,
       KC_LSFT,  KC_Z,    KC_X,      KC_C,     KC_V,            KC_B,     KC_N,   KC_M,           KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_PGUP, KC_UP,
-      KC_LCTL,  LT(_FNC,A(KC_GRV)), KC_LGUI, KC_LALT, KC_SPC, KC_SPC, LT(_NUM,JP_MHEN), KC_RALT, KC_RCTL, KC_CAPS,  KC_DEL,   KC_LEFT, KC_DOWN, KC_RGHT
+      KC_LCTL,  LT(_NUM,A(KC_GRV)), KC_LGUI, KC_LALT, KC_SPC, KC_SPC, LT(_FNC,JP_MHEN), KC_RALT, KC_RCTL, KC_CAPS,  KC_DEL,   KC_LEFT, KC_DOWN, KC_RGHT
     ),
  /* Fnc
    * ,-----------------------------------------.   ,------------------------------------------------.
